a2939c9cbab52dfcfc8d9f33af07d49d617ebb0a
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2021-06-24 18:07:12 +0300

    short name

--- src/components/complayer.vue
+++ src/components/complayer.vue
@@ -9,14 +9,20 @@
       Тег audio не поддерживается вашим браузером.
       <a :href="file">Скачайте музыку</a>.
     </audio>
-    <span class="playlist__debug">{{ file }}</span>
+    <span class="playlist__debug">{{ shortName }}</span>
   </div>
 </template>
 <script>
 export default {
   props: {
     file: {
-      type: String
+      type: String,
+      default: 'none'
+    }
+  },
+  computed: {
+    shortName () {
+      return this.file.substring(this.file.length - 17)
     }
   }
 }
